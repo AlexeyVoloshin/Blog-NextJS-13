b79133445ff9cc0e715dab458f4f8820255bc218
Alexey Voloshin <alexeyvoloshin@User-Mac.local>
2023-06-09 12:12:12 +0300

    added footer and header component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "About | Next App"
+}
+
 export default function About() {
-    return <h1>About page</h1>
+    return <h3>Select subitem</h3>
 }
---
+++ app/blog/error.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export default function ErrorWrapper({ error }: { error: Error }) {
+    return <h1>Opps!!! {error.message}</h1>
+}
+
