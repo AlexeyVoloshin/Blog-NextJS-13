dbde844219c54ae1a372e0562c196869ed532234
Alexey Voloshin <alexeyvoloshin@User-Mac.local>
2023-06-10 15:46:48 +0300

    added api

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { headers, cookies } from "next/headers";
+import { redirect } from 'next/navigation';
+
+export async function DELETE(req:Request, { params }: { params: { id: string}}) {
+    const id = params.id;
+
+    const headerList = headers();
+    const type = headerList.get('Content-Type');
+
+    const cookiesList = cookies();
+    const coo1 = cookiesList.get('Cookie_1')?.value;
+
+    // logic delete post
+    // redirect('/blog');
+
+    return NextResponse.json({ id, type, coo1 });
+}
